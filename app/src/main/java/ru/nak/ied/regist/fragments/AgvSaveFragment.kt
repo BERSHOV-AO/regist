ec667a0e196f0f46ba1000b0db2629ad85c456a2
Aleksey Bershov <bershov.aleksey@bk.ru>
2024-07-24 13:59:34 +0300

    refactor code, add pass delete agv+agv_to

--- app/src/main/java/ru/nak/ied/regist/fragments/AgvSaveFragment.kt
+++ app/src/main/java/ru/nak/ied/regist/fragments/AgvSaveFragment.kt
@@ -56,11 +56,11 @@ class AgvSaveFragment : BaseFragment() {
         val buttonDeleteAgv = view.findViewById<Button>(R.id.bDeleteAgvBySerialNum)
         val serialNumDeleteET = view.findViewById<EditText>(R.id.etSerialNumAgvDelete)
 
-
         buttonSaveAgv.setOnClickListener {
             val dialogView = layoutInflater.inflate(R.layout.dialog_password, null)
             val etDialogPassword = dialogView.findViewById<EditText>(R.id.etDialogPassword)
 
+            //-----------------------------------------pass-----------------------------------------
             AlertDialog.Builder(requireContext())
                 .setTitle("Введите пароль")
                 .setView(dialogView)
@@ -68,6 +68,7 @@ class AgvSaveFragment : BaseFragment() {
                     val enteredPassword = etDialogPassword.text.toString()
                     if (enteredPassword == "121286") {
                         Toast.makeText(context, "Пароль верный", Toast.LENGTH_SHORT).show()
+                        //--------------------------------------------------------------------------
 
                         val thisCurrentTime: String = getCurrentTime()
                         /**
@@ -140,48 +141,74 @@ class AgvSaveFragment : BaseFragment() {
         }
 
         buttonDeleteAgv.setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch {
-                val listAgv = mainApi.getAllAGV()
 
-                val sn = serialNumDeleteET.text.toString().trim()
+            val dialogView = layoutInflater.inflate(R.layout.dialog_password, null)
+            val etDialogPassword = dialogView.findViewById<EditText>(R.id.etDialogPassword)
 
-                if (sn.isEmpty()) {
-                    Toast.makeText(
-                        context, "Поле незаполнено",
-                        Toast.LENGTH_LONG
-                    ).show()
-                } else {
-                    // Проверка наличия AGV с указанным серийным номером
-                    val agvToDelete = listAgv.find { it.serialNumber == sn }
+            //-----------------------------------------pass-----------------------------------------
+            AlertDialog.Builder(requireContext())
+                .setTitle("Введите пароль")
+                .setView(dialogView)
+                .setPositiveButton("OK") { dialog, _ ->
+                    val enteredPassword = etDialogPassword.text.toString()
+                    if (enteredPassword == "121286") {
+                        Toast.makeText(context, "Пароль верный", Toast.LENGTH_SHORT).show()
+                        //--------------------------------------------------------------------------
 
-                    if (agvToDelete != null) {
-                        // Если AGV найден, выполняем удаление
-                        try {
-                            mainApi.deleteAgvBySerialNumber(sn) // Предполагается, что deleteAGV - метод API для удаления
-                            mainApi.deleteAgvTOBySerialNumber(sn) //
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val listAgv = mainApi.getAllAGV()
 
-                            Toast.makeText(
-                                context, "AGV с серийным номером $sn успешно удален",
-                                Toast.LENGTH_LONG
-                            ).show()
+                            val sn = serialNumDeleteET.text.toString().trim()
 
-                        } catch (e: Exception) {
+                            if (sn.isEmpty()) {
+                                Toast.makeText(
+                                    context, "Поле незаполнено",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            } else {
+                                // Проверка наличия AGV с указанным серийным номером
+                                val agvToDelete = listAgv.find { it.serialNumber == sn }
 
-                            Log.d("MyLog", "error:  ${e.message}")
-                            Toast.makeText(
-                                context, "Ошибка sn: $sn : ${e.message}",
-                                Toast.LENGTH_LONG
-                            ).show()
+                                if (agvToDelete != null) {
+                                    // Если AGV найден, выполняем удаление
+                                    try {
+                                        mainApi.deleteAgvBySerialNumber(sn) // Предполагается, что deleteAGV - метод API для удаления
+                                        mainApi.deleteAgvTOBySerialNumber(sn) //
+
+                                        serialNumDeleteET.text.clear()
+
+                                        Toast.makeText(
+                                            context, "AGV с серийным номером $sn успешно удален",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+
+                                    } catch (e: Exception) {
+
+                                        Log.d("MyLog", "error:  ${e.message}")
+                                        Toast.makeText(
+                                            context, "Ошибка sn: $sn : ${e.message}",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                } else {
+                                    // Если AGV не найден
+                                    serialNumDeleteET.text.clear()
+                                    Toast.makeText(
+                                        context, "AGV с серийным номером $sn не найден",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            }
                         }
                     } else {
-                        // Если AGV не найден
-                        Toast.makeText(
-                            context, "AGV с серийным номером $sn не найден",
-                            Toast.LENGTH_LONG
-                        ).show()
+                        // Пароль неверный, показать сообщение об ошибке
+                        Toast.makeText(context, "Неверный пароль", Toast.LENGTH_SHORT).show()
                     }
+                    dialog.dismiss()
                 }
-            }
+                .setNegativeButton("Отмена") { dialog, _ ->
+                    dialog.dismiss()
+                }.show()
         }
         return view
     }
